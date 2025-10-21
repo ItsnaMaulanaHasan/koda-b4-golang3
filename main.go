c0c4184f3815bd89dadcb2b03df10dc43127249a
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-22 00:47:35 +0700

    feat: added loop to adjust interactivity

--- main.go
+++ main.go
@@ -16,9 +16,17 @@ func searchPerson(users []string, name string) string {
 
 func main()  {
 	users := []string{"Itsna", "Federus", "Yoga", "Fiki", "Sidik", "Ari"}
-	var inputNama string
-	fmt.Print("Enter the name you want to search for: ")
-	fmt.Scan(&inputNama)
-	result := searchPerson(users, inputNama)
-	fmt.Println(result)
+	loop := true
+
+	for loop {
+		var inputNama string
+		fmt.Print("Enter the name you want to search for (0. exit): ")
+		fmt.Scan(&inputNama)
+		if (inputNama != "0"){
+			result := searchPerson(users, inputNama)
+			fmt.Println(result)
+		} else {
+			loop = false
+		}
+	}
 }
